07209a4593dc422c13dccf7ebed1bd6b39dfc720
datla suji <datlasuji7@gmail.com>
2026-01-06 23:51:33 +0530

    Add files via upload

---
+++ day_1/level1.py
@@ -0,0 +1,36 @@
+
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 3)            # Floor division operator(//)
+
+
+
+print(type("suji"))
+print(type("datla"))
+print(type("india"))
+print(type("iam enjoy 30 day of python"))
+
+
+
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(['Asabeneh','python','finland'])
+print(type("suji"))
+print(type("datla"))
+print(type("india"))
+
+
+
+import math
+x1, y1 = 2,3
+x2, y2 = 10,8
+
+distance = math.sqrt((x2 - x1)**2 +(y2 - y1)**2)
+print("Euclidean Distance:", distance)
